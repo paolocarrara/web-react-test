6a2572b4bb09698724cc1e3c5370a914ee66c4c5
pcarrara <paolocarraratux@gmail.com>
2021-09-18 20:55:35 -0300

    Creates the Card component and enhances the SummaryCard component.

--- src/components/summary-card/SummaryCard.tsx
+++ src/components/summary-card/SummaryCard.tsx
@@ -1,53 +1,119 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import './SummaryCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import Card from '../card/Card';
 
 interface SummaryCardProps {
     total: number
     profitability: number
     gain: number
     cdi: number
+    currency?: string
+    locale?: string
 }
 
-const SummaryCard = (props: SummaryCardProps) => {
-    return (
-        <div className={'summary-card'}>
-            <div className="header">
-                <div className="title">
-                    Seu resumo
-                </div>
+class SummaryCard extends Component <SummaryCardProps> {
+    static defaultProps: SummaryCardProps = {
+        total: 0,
+        profitability: 0,
+        gain: 0,
+        cdi: 0,
+        currency: 'BRL',
+        locale: 'pt-BR'
+    }
 
-                <div className="more">
-                    ...
-                </div>
-            </div>
+    handleEllipsesButtonOnClick = () => {}
 
-            <div className={'investment'}>
-                <div className="title">
-                    Valor investido
-                </div>
-                <div className="value">
-                    {props.total}
-                </div>
-            </div>
+    handleSeeMoreButtonOnClick = () => {}
 
-            <div className={'summary'}>
-                <div className="row">
-                    <span className="title">Rentabilidade/mes</span>
-                    <div className="value">{props.profitability}</div>
-                </div>
+    convertToCurrency = (value: number) => {
+        const locales = 'pt-BR';
+        const options = {
+            style: 'currency',
+            currency: 'BRL',
+        }
 
-                <div className="row">
-                    <span className="title">CDI</span>
-                    <div className="value">{props.cdi}</div>
-                </div>
+        return this.numberConverter(locales, options, value)
+    }
 
-                <div className="row">
-                    <span className="title">Ganho/mes</span>
-                    <div className="value">{props.gain}</div>
+    convertToPercent = (value: number, minimumFractionDigits: number = 2) => {
+        const locales = 'pt-BR';
+        const options = {
+            style: 'percent',
+            minimumFractionDigits,
+        }
+
+        value = value/100;
+
+        return this.numberConverter(locales, options, value)
+    }
+
+    numberConverter = (locales: string, options: Intl.NumberFormatOptions, value: number) => {
+        const formatter = new Intl.NumberFormat(locales, options)
+
+        return formatter.format(value)
+    }
+
+    render() {
+        return (
+            <Card>
+                <div className={'summary-card'}>
+                    <div className="header">
+                        <div className="row main">
+                            <div className="title">
+                                Seu resumo
+                            </div>
+
+                            <div className="more">
+                                <button onClick={this.handleEllipsesButtonOnClick}>
+                                    <FontAwesomeIcon icon={faEllipsisV} />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="body">
+                        <div className={'row investment'}>
+                            <div className="title">
+                                Valor investido
+                            </div>
+                            <div className="value">
+                                {this.convertToCurrency(this.props.total)}
+                            </div>
+                        </div>
+
+                        <div className={'row summary'}>
+                            <div className="row">
+                                <span className="title">Rentabilidade/mes</span>
+                                <div className="value">{this.convertToPercent(this.props.profitability, 3)}</div>
+                            </div>
+
+                            <div className="row">
+                                <span className="title">CDI</span>
+                                <div className="value">{this.convertToPercent(this.props.cdi)}</div>
+                            </div>
+
+                            <div className="row">
+                                <span className="title">Ganho/mes</span>
+                                <div className="value">{this.convertToCurrency(this.props.gain)}</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr/>
+
+                    <div className="footer">
+                        <div className={'row actions'}>
+                            <button onClick={this.handleSeeMoreButtonOnClick}>
+                                Ver mais
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            </Card>
+        )
+    }
 }
 
 export default SummaryCard
